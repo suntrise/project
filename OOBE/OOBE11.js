a6fd9faaa8af36a9838da39e3d2b6955ba539272
suntrise <89229642+suntrise@users.noreply.github.com>
2023-04-15 17:19:48 +0800

    Update OOBE11.js

--- OOBE/OOBE11.js
+++ OOBE/OOBE11.js
@@ -199,16 +199,20 @@ window.onload = function(){
 document.onkeydown = hotkey;
 };
 
+//检测是否为手机端
 if (window.location.toString().indexOf('pref=padindex') != -1) { } else {
-            if (/AppleWebKit.*Mobile/i.test(navigator.userAgent) || (/MIDP|SymbianOS|NOKIA|SAMSUNG|LG|NEC|TCL|Alcatel|BIRD|DBTEL|Dopod|PHILIPS|HAIER|LENOVO|MOT-|Nokia|SonyEricsson|SIE-|Amoi|ZTE/.test(navigator.userAgent))) {
-                if (window.location.href.indexOf("?mobile") < 0) {
-                    try {
-                        if (/Android|Windows Phone|webOS|iPhone|iPod|BlackBerry/i.test(navigator.userAgent)) {
-                            alert("建议使用电脑端，手机端不能达到最佳显示效果")
-                        } else if (/iPad/i.test(navigator.userAgent)) {
-                        } else {
+     if (/AppleWebKit.*Mobile/i.test(navigator.userAgent) || (/MIDP|SymbianOS|NOKIA|SAMSUNG|LG|NEC|TCL|Alcatel|BIRD|DBTEL|Dopod|PHILIPS|HAIER|LENOVO|MOT-|Nokia|SonyEricsson|SIE-|Amoi|ZTE/.test(navigator.userAgent))) {
+         if (window.location.href.indexOf("?mobile") < 0) {
+            try {
+                if (/Android|Windows Phone|webOS|iPhone|iPod|BlackBerry/i.test(navigator.userAgent)) {
+                    alert("建议使用电脑端，手机端不能达到最佳显示效果")
+                } 
+                else if (/iPad/i.test(navigator.userAgent)) {
+                        } 
+                else {
                         }
-                    } catch (e) {
+                    } 
+                catch (e) {
                     }
                 }
             }
