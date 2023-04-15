b46e390a81793cc5a2c3d2037251d624841a1197
suntrise <89229642+suntrise@users.noreply.github.com>
2023-04-15 17:22:58 +0800

    Update OOBE81.js

--- OOBE/OOBE81.js
+++ OOBE/OOBE81.js
@@ -166,13 +166,13 @@ window.onload = function(){
 document.onkeydown = hotkey;
 };
 
-//检测是否为手机端
+//检测是否为移动端
 if (window.location.toString().indexOf('pref=padindex') != -1) { } else {
      if (/AppleWebKit.*Mobile/i.test(navigator.userAgent) || (/MIDP|SymbianOS|NOKIA|SAMSUNG|LG|NEC|TCL|Alcatel|BIRD|DBTEL|Dopod|PHILIPS|HAIER|LENOVO|MOT-|Nokia|SonyEricsson|SIE-|Amoi|ZTE/.test(navigator.userAgent))) {
          if (window.location.href.indexOf("?mobile") < 0) {
             try {
                 if (/Android|Windows Phone|webOS|iPhone|iPod|BlackBerry/i.test(navigator.userAgent)) {
-                    alert("建议使用电脑端，手机端不能达到最佳显示效果")
+                    alert("建议使用电脑打开，移动端不能达到最佳显示效果")
                 } 
                 else if (/iPad/i.test(navigator.userAgent)) {
                         } 
