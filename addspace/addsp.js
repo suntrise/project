c95c8fce39f9ccbc0641a87c1480d535c9ac8723
suntrise <89229642+suntrise@users.noreply.github.com>
2023-05-21 13:03:17 +0800

    Update addsp.js

--- addspace/addsp.js
+++ addspace/addsp.js
@@ -1,40 +1,47 @@
 var str;
-var all = test = "";
-var a = b = 0;
+var all = test = a = b = "";
+var a = b = j = k = 0;
 
-//var ct = setInterval(addsp,1)
+/* 测试
+var te = " "
+confirm(te.charCodeAt())
+var ct = setInterval(addsp,1)
+*/
 
 function addsp() {
 str = type.value
-str = str.split('')
 for (let i in str) {
-var al = str[i]
-var bl = str[Math.floor(i-1)]
+al = str[i]
 a = al.charCodeAt()
-if (bl != undefined) {
-b = bl.charCodeAt()
-}
-
 if((a >= 8216 && a <= 8223)||(a >= 12288 && a <= 12351)||(a >= 65281 && a <= 65374)){
-	a=2;
-}
+	j=2;
+}//标点符号为 2
 else if (a >= 11904 && a <= 193565){
-	a=1;
-}
+	j=1;
+}// CJK 字符为 1
+else if(a == 32){
+	j=3;
+}//空格为 3
 else{
-	a=0;
+	j=0;
 }
+if (i>=2) {
+bl = str[Math.floor(i-1)]
+b = bl.charCodeAt()
 if((b >= 8216 && b <= 8223)||(b >= 12288 && b <= 12351)||(b >= 65281 && b <= 65374)){
-	b=2;
-}
+	k=2;
+}//标点符号为 2
 else if (b >= 11904 && b <= 193565){
-	b=1;
-}
-
+	k=1;
+}// CJK 字符为 1
+else if(b == 32){
+	k=3;
+}//空格为 3
 else{
-	b=0;
+	k=0;
 }
-if (a+b == 1) {
+}
+if (j+k == 1) {
 	al = " "+al;
 }
 
@@ -43,4 +50,5 @@ all += al;
 
 }
 	all=""
+	j=k=0;
 }
